e973dc3e49fc9a5181618e1ef502df4274bd4c9f
ccbrantley <ccbrantley@gtcc.edu>
2020-08-28 12:48:58 -0400

    Editing requirements.txt

--- src/CovidAnalytics.py
+++ src/CovidAnalytics.py
@@ -1,10 +1,11 @@
 from CovidData import *
-
+import pandas as pd
 def main():
-    print(CovidData.getCovidPrimaryData())
-    print(CovidData.getCovidPrimaryData()\
-          .info("verbose"))
-    print(CovidData.getCountyEmployment())
-    print(CovidData.getCountyHospital())
+    print(pd.show_versions())
+    #print(CovidData.getCovidPrimaryData())
+    #print(CovidData.getCovidPrimaryData()\
+    #      .info("verbose"))
+    #print(CovidData.getCountyEmployment())
+    #print(CovidData.getCountyHospital())
     
 main()
